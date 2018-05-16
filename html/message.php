6f817bfff4ef8725a93d5c37ce4d5fd6d471c4f3
hsg1005 <31918475+hsg1005@users.noreply.github.com>
2018-05-17 01:59:56 +0900

    last game timestamp to date

--- html/message.php
+++ html/message.php
@@ -1,36 +1,30 @@
 <?php
 
+
 $servername = "servername";
 $username = "username";
 $password = "password";
 $dbname = "dbname";
-
 // DB 접속하기
 $conn = mysqli_connect($servername, $username, $password, $dbname);
-
 $input = json_decode(file_get_contents('php://input'), true);
 $input_message = $input['content'];
 $user_key = $input['user_key'];
-
 // message_log 테이블에 사용자 메시지와 사용자 키를 저장함
 $sql = "INSERT INTO message_log (message, user_key) VALUES ('$input_message', '$user_key')";
-
 // 한글 처리 및 쿼리문 실행
 mysqli_query($conn, 'set names utf8');
 $conn->query($sql);
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 //한글 깨짐 방지하는 UTF-8인코딩
 //header("Content-Type: text/html; charset=UTF-8");
-
 //자신의 api_key
 $api_key = "api_key";
 //대문자를 소문자로 변환
 //양쪽 공백 제거
 $username_tmp = strtolower(trim($input_message));
 //summonerNames //usernames에서 공백(" ")을 ""로 대체
-$username = preg_replace("/\s+/", "", $username_tmp); 
+$username = preg_replace("/\s+/", "", $username_tmp);
 
 //SUMMONER-V3 {summonerName}
 $summoners_by_name =
@@ -38,6 +32,7 @@ json_decode(httpGet("https://kr.api.riotgames.com/lol/summoner/v3/summoners/by-n
 
 $summoner_name = $summoners_by_name['name'];
 $summoner_id = $summoners_by_name['id'];
+$summoner_account_id = $summoners_by_name['accountId'];
 $summoner_level = $summoners_by_name['summonerLevel'];
 
 //LEAGUE-V3 {summonerId}
@@ -48,55 +43,48 @@ json_decode(httpGet("https://kr.api.riotgames.com/lol/league/v3/positions/by-sum
 $active_games_by_summoner =
 json_decode(httpGet("https://kr.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/".$summoner_id."?api_key=".$api_key), true);
 
-// 여기서부터 출력 시작한다.
+// MATCH-V3 {accountId}
+$matchlists_by_account =
+json_decode(httpGet("https://kr.api.riotgames.com/lol/match/v3/matchlists/by-account/".$summoner_account_id."?api_key=".$api_key), true);
 
+//가장 최근 게임 날짜 정보
+$timestamp = $matchlists_by_account['matches'][0]['timestamp'];
+$date = date("Y-m-d H:i:s", $timestamp/1000);
+
+
+// 여기서부터 출력 시작한다.
 //인게임 정보
 $output_message = " ";
-
 if($active_games_by_summoner['status']['status_code'] == 404){
 	$output_message = $output_message. "(하하)인게임 정보가 업습니다.\n\n";
 }
 else{
 	$output_message = $output_message. "(하하)인게임 정보\n\n";
-
 	$participants = $active_games_by_summoner['participants'];
-
 	for($i = 0; $i < 10; $i++){
-
 		if($i == 5)
 			$output_message = $output_message. "VS\n";
-
 		$champion_id = $participants[$i]['championId'];
 		//LOL-STATIC-DATA-V3 {id}
 		$champions =
 		json_decode(httpGet("https://kr.api.riotgames.com/lol/static-data/v3/champions/".$champion_id."?locale=ko_KR&api_key=".$api_key), true);
-
 		$output_message = $output_message. " " . $champions['name'] . " " . $participants[$i]['summonerName'] . "\n";
 	}
 	$output_message = $output_message. "\n";
-
 }
-
-
 if($summoner_id == "" || $summoner_id['status']['status_code'] == 404 ){
 	send_text("존재하지 않는 아이디입니다(헉)");
 }
-
 $output_message = $output_message. "소환사명 : " . $summoner_name. "\n";
-$output_message = $output_message. "레벨 : " . $summoner_level. "\n\n";
-
-
-
+$output_message = $output_message. "레벨 : " . $summoner_level. "\n";
+$output_message = $output_message. "최근 게임 : " . $date . "\n\n";
 
 if($league_position_by_summoner['status']['status_code'] == 404 ){
 	$output_message = $output_message. "매치기록이 없습니다(헉)";
 	send_text($output_message);
 }
-
-
 //현재 시즌 리그정보
 for($i = 0; $i < count($league_position_by_summoner); $i++){
-
 	$league_queue_type =  $league_position_by_summoner[$i]['queueType'];
 	
 	if($league_queue_type == "RANKED_FLEX_SR"){
@@ -105,7 +93,6 @@ for($i = 0; $i < count($league_position_by_summoner); $i++){
 	elseif($league_queue_type == "RANKED_SOLO_5x5"){
 		$output_message = $output_message. "<솔랭>\n";
 	}
-
 	$league_wins = $league_position_by_summoner[$i]['wins'];
 	$league_losses = $league_position_by_summoner[$i]['losses'];
 	$league_rank = $league_position_by_summoner[$i]['rank'];
@@ -117,10 +104,7 @@ for($i = 0; $i < count($league_position_by_summoner); $i++){
 	$output_message = $output_message. "전적 : " . $league_wins . "승 " . $league_losses . "패 " .
 	"(승률 : " . round($league_wins/($league_wins+$league_losses)*100,2) . "%)\n\n"; //승률 소수점 2자리 반올림
 }
-
-
 send_text($output_message);
-
 
 function httpGet($url)
 {
